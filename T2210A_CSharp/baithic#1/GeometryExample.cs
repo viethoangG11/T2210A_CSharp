86ef93336385c4ef3cb8ae8f297ac1c07ff666bd
viethoangG11 <hoangyla11112004@gmail.com>
2023-08-30 15:06:15 +0700

    Bài thi C#

---
+++ T2210A_CSharp/baithic#1/GeometryExample.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2210A_CSharp.baithic_
+{
+    internal class GeometryExample
+    {
+        public double BaseArea;
+        public double LateralArea;
+        public double TotalArea;
+        public double Volume;
+
+        public void Process(double radius, double height)
+        {
+            BaseArea = radius * radius * Math.PI;
+            LateralArea = 2 * Math.PI * radius * height;
+            TotalArea = 2 * Math.PI * radius * (height + radius);
+            Volume = Math.PI * radius * radius * height;
+        }
+
+        public void Result()
+        {
+            Console.WriteLine("Base: {0} | Lateral: {1} | Total: {2} | Volume: {3}", BaseArea, LateralArea, TotalArea, Volume);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the dimensions of the GeometryExample");
+            Console.Write("Radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Height: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            GeometryExample c = new GeometryExample();
+            c.Process(radius, height);
+            c.Result();
+        }
+    }
+}
---
+++ T2210A_CSharp/baithic#1/baithic#2/Animal.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2210A_CSharp.baithic_1.baithic_2
+{
+    internal class Animal
+    {
+        protected int weight;
+        protected string name;
+
+        public void Show()
+        {
+            Console.WriteLine("Name: {0}, Weight: {1}", name, weight);
+        }
+
+        public void SetMe(int weight, string name)
+        {
+            this.weight = weight;
+            this.name = name;
+        }
+    }
+
+    class Lion : Animal
+    {
+        public Lion(int weight, string name)
+        {
+            SetMe(weight, name);
+        }
+    }
+
+    class Tiger : Animal
+    {
+        public Tiger(int weight, string name)
+        {
+            SetMe(weight, name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Tiger t = new Tiger(100, "Tiger");
+             Lion l = new Lion(200, "Lion");
+
+            t.Show();
+            l.Show();
+        }
+    }
+}
